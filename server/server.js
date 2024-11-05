20248871074197332f02f993e61c2a551fac7bb4
Nimesh Poudel <nimesh.pchm@lendenclub.com>
2024-11-05 12:15:25 -0500

    node js setuo

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import fs from "fs";
-
+import cors from "cors";
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.post("/register", (req, res) => {
